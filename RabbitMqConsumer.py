8872ec05874f8dac3adb69b3579ad49c799d1a3e
996Worker <shaotienlee@foxmail.com>
2023-09-06 12:55:32 +1000

    feat: improved consumer

--- RabbitMqConsumer.py
+++ RabbitMqConsumer.py
@@ -1,4 +1,10 @@
+import ssl
+
 import pika
+
+import argparse
+
+from config import rabbitmq_user, rabbitmq_password, rabbitmq_broker_id, region, vhost
 
 
 def callback(ch, method, properties, body):
@@ -10,16 +16,20 @@ def receive_data(queue_name):
         helper to receive message from remote rabbitmq
         :return: data
     """
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(
-            host='b-d6c2e027-eebd-49a7-90b3-1c587949a894.mq.ca-central-1.amazonaws.com',
-            port=5671,
-            credentials=None,
-        )
-    )
+
+    # SSL Context for TLS configuration of Amazon MQ for RabbitMQ
+    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    ssl_context.set_ciphers('ECDHE+AESGCM:!ECDSA')
+
+    url = f"amqps://{rabbitmq_user}:{rabbitmq_password}@{rabbitmq_broker_id}.mq.{region}.amazonaws.com:5671/{vhost}"
+
+    parameters = pika.URLParameters(url)
+    parameters.ssl_options = pika.SSLOptions(context=ssl_context)
+
+    connection = pika.BlockingConnection(parameters)
 
     channel = connection.channel()
-    channel.queue_declare(queue=queue_name)
+    # channel.queue_declare(queue=queue_name)
     channel.basic_consume(queue=queue_name,
                           on_message_callback=callback,
                           auto_ack=True)
@@ -28,4 +38,10 @@ def receive_data(queue_name):
 
 
 if __name__ == '__main__':
-    receive_data('demo-queue')
+    parser = argparse.ArgumentParser(description='Receive data from a RabbitMQ queue.')
+    parser.add_argument('queue_name',
+                        type=str,
+                        help='The name of the queue to consume data from.')
+    args = parser.parse_args()
+
+    receive_data(args.queue_name)
